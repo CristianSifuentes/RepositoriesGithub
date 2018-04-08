53261854d1fc4d29853d9d81a95327ccd9cf6774
CristianSifuentes <sifuentescovarrubiasitsl@gmail.com>
2018-04-07 20:10:41 -0500

    CSC 20180407: Uso del mat-grid-list

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { GithubService } from './../../services/github.service';
+import { ObservableMedia } from '@angular/flex-layout';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
 
 @Component({
   selector: 'app-home',
@@ -7,8 +10,13 @@ import { GithubService } from './../../services/github.service';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  
+  
   public repositories;
-  constructor(private _githubService :GithubService) {
+  public cols: Observable<number>;
+
+
+  constructor(private _githubService :GithubService,  private observableMedia: ObservableMedia) {
     this._githubService.getRepositories().subscribe(
       // the first argument is a function which runs on success
       data => { this.repositories = data},
@@ -20,7 +28,29 @@ export class HomeComponent implements OnInit {
 
    }
 
-  ngOnInit() {
+   ngOnInit() {
+    // set cols
+    if (this.observableMedia.isActive('xs')) {
+      this.cols = Observable.of(1);
+    } else if (this.observableMedia.isActive('sm') || this.observableMedia.isActive('md')) {
+      this.cols = Observable.of(2);
+    } else if (this.observableMedia.isActive('lg') || this.observableMedia.isActive('xl')) {
+      this.cols = Observable.of(4);
+    }
+    // observe changes
+    this.observableMedia.asObservable()
+      .subscribe(change => {
+        switch (change.mqAlias) {
+          case 'xs':
+            return this.cols = Observable.of(1);
+          case 'sm':
+          case 'md':
+            return this.cols = Observable.of(2);
+          case 'lg':
+          case 'xl':
+            return this.cols = Observable.of(4);
+        }
+      });
   }
 
 }
--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -7,7 +7,7 @@ export class GithubService {
   private username = "CristianSifuentes";
   private client_id = "29431fe2ad6a2b43bf4c";
   private client_secret = "e3713c5c4c40d5bb5867e085f9ab3b6642cd7da5";
-  private url = "https://api.github.com/users/CristianSifuentes/repos"
+  private url = "https://api.github.com/users/CristianSifuentes/following"
   constructor( private http: HttpClient) {
     console.log("Esta en el servicio GithubService");
         this.getRepositories().subscribe(data => {
