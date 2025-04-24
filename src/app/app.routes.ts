2e523c723ade0c949fdb4c4af03f9ae91c342f68
CristianSifuentes <sifuentescovarrubiasitsl@gmail.com>
2025-04-24 12:47:43 -0600

    Working with flex-flow

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { CSSGridLayoutComponent } from './cssgrid-layout/cssgrid-layout.component';
 import { CSSFlexboxComponent } from './cssflexbox/cssflexbox.component';
+import { AppComponent } from './app.component';
 
 
 export const routes: Routes = [
+  {
+    path: '', component: AppComponent
+
+  },
   {
     path: 'CSSGrid', component: CSSGridLayoutComponent
 
