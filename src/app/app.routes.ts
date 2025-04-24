0c467cd2179925abcf7c24be61f3d9a3e75d3953
CristianSifuentes <sifuentescovarrubiasitsl@gmail.com>
2025-04-24 14:16:19 -0600

    Creating new components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,12 @@ import { Routes } from '@angular/router';
 import { CSSGridLayoutComponent } from './cssgrid-layout/cssgrid-layout.component';
 import { CSSFlexboxComponent } from './cssflexbox/cssflexbox.component';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 
 export const routes: Routes = [
   {
-    path: '', component: AppComponent
+    path: '', component: HomeComponent
 
   },
   {
