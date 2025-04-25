fd0450de643ab70078516e94cfb02ecd8379ccb8
CristianSifuentes <sifuentescovarrubiasitsl@gmail.com>
2025-04-25 14:26:13 -0600

    Improve format

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+// toolbar
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+//sidenav
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+//matcard
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-root',
-  // imports: [RouterOutlet],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatSidenavModule, MatListModule, MatCardModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'you-app-name';
+  protected readonly isMobile = signal(true);
+
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import {provideHttpClient} from '@angular/common/http';
+import {VERSION as MAT_VERSION, provideNativeDateAdapter} from '@angular/material/core';
+console.info('Angular Material version', MAT_VERSION.full);
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+
+bootstrapApplication(AppComponent, {
+  providers: [ provideHttpClient(), provideNativeDateAdapter()],
+}).catch(err => console.error(err));
